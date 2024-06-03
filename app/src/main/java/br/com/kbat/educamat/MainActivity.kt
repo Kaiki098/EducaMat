47a35ca25d456b4938399af6ecdac8005f1a82f6
Kaiki Alvarenga <kaikialvarengasouza098@gmail.com>
2024-06-03 20:17:03 -0300

    Adding splash screen

--- app/src/main/java/br/com/kbat/educamat/MainActivity.kt
+++ app/src/main/java/br/com/kbat/educamat/MainActivity.kt
@@ -11,12 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import br.com.kbat.educamat.presentation.theme.EducaMatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
+
         setContent {
             EducaMatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->

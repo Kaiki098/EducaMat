2ffb98b90f84886eb7a5e315f60d8f2477104733
Kaiki Alvarenga de Souza <kaikialvarengasouza098@gmail.com>
2024-07-24 23:41:47 -0300

    chore: create question screen

--- app/src/main/java/br/com/kbat/educamat/presentation/screen/question/QuestionScreen.kt
+++ app/src/main/java/br/com/kbat/educamat/presentation/screen/question/QuestionScreen.kt
@@ -1,18 +1,29 @@
 package br.com.kbat.educamat.presentation.screen.question
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.kbat.educamat.R
+import br.com.kbat.educamat.presentation.screen.questions.QuestionChoice
+import br.com.kbat.educamat.presentation.theme.EducaMatTheme
+import br.com.kbat.educamat.presentation.theme.Orange
 
 @Composable
 fun QuestionScreen(modifier: Modifier = Modifier, onBackClick: () -> Unit) {
@@ -25,14 +36,46 @@ fun QuestionScreen(modifier: Modifier = Modifier, onBackClick: () -> Unit) {
         )
 
         Column(
-            modifier,
-            verticalArrangement = Arrangement.Center,
+            modifier.padding(horizontal = 30.dp, vertical = 60.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Question Screen")
-            Button(onClick = { onBackClick() }) {
-                Text(text = "Voltar")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.3f)
+                    .border(width = 10.dp, color = Orange, shape = RoundedCornerShape(20))
+                    .background(color = Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Pergunta", fontSize = 24.sp)
             }
+
+            Column(
+                modifier = Modifier
+                    .weight(0.7f)
+                    .padding(vertical = 60.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                List(4) {
+                    QuestionChoice(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        onClick = { /*TODO*/ },
+                        text = "Resposta"
+                    )
+                }
+            }
+
         }
+
+    }
+}
+
+@Preview
+@Composable
+private fun QuestionScreenPreview() {
+    EducaMatTheme {
+        QuestionScreen(modifier = Modifier.fillMaxSize(), onBackClick = {})
     }
 }
---
+++ app/src/main/java/br/com/kbat/educamat/presentation/screen/questions/Components.kt
@@ -0,0 +1,32 @@
+package br.com.kbat.educamat.presentation.screen.questions
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import br.com.kbat.educamat.presentation.theme.EducaMatTheme
+
+@Composable
+fun QuestionChoice(modifier: Modifier = Modifier, onClick: () -> Unit, text: String) {
+    Button(modifier = modifier, shape = RoundedCornerShape(20), onClick = { onClick() }) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 24.sp,
+            fontFamily = MaterialTheme.typography.bodyLarge.fontFamily
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun QuestionChoicePreview() {
+    EducaMatTheme {
+        QuestionChoice(onClick = {}, text = "456")
+    }
+}
